a32a8fdb3c2e14ab4b1d258e8229b33b3cecbce0
Kyle Sutherland-Cash <khs26@cam.ac.uk>
2015-09-04 18:36:55 +0100

    Added I/O routines for GMIN and AMBER file formats.

---
+++ rotamer/io/amber.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os.path
+import math
+
+def read_amber_restart(filename):
+    field_length = 12
+    coords = []
+    with open(filename, "r") as coords_file:
+        # Throw away the first line, since it just contains the name of the molecule.
+        coords_file.readline()
+        # The next line contains the number of atoms.
+        number_of_atoms = int(coords_file.readline())
+        # Later lines contain coordinates in 12-character wide fields.
+        for line in coords_file:
+            line = line.rstrip()
+            coords += map(float, [line[i:i + field_length] for i in range(0, len(line), field_length)])
+    # If the number of coordinates is not equal to 3 * number of atoms, raise a RuntimeError.
+    if len(coords) != number_of_atoms * 3:
+        raise RuntimeError("Number of coordinates in coords file and number of atoms are inconsistent.")
+    return np.array(coords)
+
+def write_amber_restart(filename, coords):
+    coords = np.reshape(coords, (-1, 3))
+    with open(filename, "w") as restart_file:
+        restart_file.write("{0}\n".format(os.path.basename(filename)))
+        restart_file.write("{0: >5d}\n".format(coords.shape[0]))
+        buff = []
+        # Coordinates are printed in the format 6F12.7. If there are an odd number
+        # of atoms, this means that we'll have one line that is 3F12.7 at the end.
+        for i, coord in enumerate(coords):
+            buff += coord.tolist()
+            if i % 2 == 1:
+                restart_file.write(("{: >12.7f}" * 6 + "\n").format(*buff))
+                buff = []
+        if buff:
+            restart_file.write(("{: >12.7f}" * 3 + "\n").format(*buff))
+
+if __name__ == "__main__":
+    write_amber_restart("test.rst", [0, 12, 15, 2, 0.5, 3, 4, 5, 6])
---
+++ rotamer/io/gmin.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def read_one_structure(lowest_file):
+    natoms = int(lowest_file.readline().strip())
+    data_line = lowest_file.readline().split()
+    index = int(data_line[3][:-1])
+    energy = float(data_line[4])
+    first_found = int(data_line[-1])
+    coords = np.zeros([natoms, 3])
+    for i in range(0, natoms):
+        coords[i] = map(float, lowest_file.readline().split()[1:])
+    return index, energy, first_found, coords
+
+def read_lowest(lowest_filename):
+    structures = []
+    with open(lowest_filename, "r") as lowest_file:
+        while True:
+            try:
+                structures.append(read_one_structure(lowest_file))
+            except ValueError:
+                # We raise a ValueError if we go too far. Not sure why this indicates end-of-file.
+                break
+    return structures
+
+def write_coords(coords, coords_filename):
+    coords = np.reshape(coords, (-1, 3))
+    with open(coords_filename, "w") as coords_file:
+        coords_file.write("{: >10d}\n".format(coords.shape[0]))
+        for coord in coords:
+            coords_file.write(("{: >20.10f}" * 3 + "\n").format(*coord))
+
+if __name__ == "__main__":
+    lowest_structs = read_lowest("/scratch/khs26/ser_lys_fe_bh/ff03_igb2/temp_0.0/rep5/output/lowest")
+    write_coords(np.array(lowest_structs[0][-1]), "test_output")
