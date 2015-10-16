201ff34185b1f5a405d780ae6071357964eba231
Kyle Sutherland-Cash <khs26@cam.ac.uk>
2015-10-16 17:13:33 +0100

    More comments and included testing for finding backbone atoms.

--- rotamer/topology/identify_backbone.py
+++ rotamer/topology/identify_backbone.py
@@ -1,14 +1,18 @@
-from read_amber_prmtop import parse_topology_file
 import networkx as nx
+
+from read_amber_prmtop import parse_topology_file
+
 
 def find_backbone_carbons(molecule):
     atoms = molecule.atoms
     carbons = [atom for atom in atoms if atom.element == 'C']
     oxygens = [atom for atom in atoms if atom.element == 'O']
     nitrogens = [atom for atom in atoms if atom.element == 'N']
+    # Carbons with three bonds, with at least one neighbouring oxygen.
     c_carbonyls = [atom for atom in carbons
                    if atoms.degree(atom) == 3
                    and list(set(nx.neighbors(atoms, atom)) & set(oxygens))]
+    # Carbons with four bonds, with at least one neighbouring carbonyl and one neighbouring nitrogen.
     c_alphas = [atom for atom in carbons
                 if atoms.degree(atom) == 4
                 and list(set(nx.neighbors(atoms, atom)) & set(c_carbonyls))
@@ -16,12 +20,17 @@ def find_backbone_carbons(molecule):
     backbone_atoms = {}
     for c_alpha in c_alphas:
         this_res = c_alpha.residue
-        backbone_atoms[this_res] = (c_alpha, list(set(nx.neighbors(atoms, c_alpha)) & set(c_carbonyls))[0])
         this_res.backbone_map = {}
         this_res.backbone_map["CA"] = c_alpha
-        this_res.backbone_map["C'"] = backbone_atoms[c_alpha.residue][1]
+        # Carbonyl neighbouring C-alpha
+        this_res.backbone_map["C'"] = list(set(nx.neighbors(atoms, c_alpha)) & set(c_carbonyls))[0]
+        # Nitrogen neighbouring C-alpha
         this_res.backbone_map["N"] = list(set(nx.neighbors(atoms, c_alpha)) & set(nitrogens))[0]
     return backbone_atoms
 
+
 if __name__ == "__main__":
     molecule = parse_topology_file("/home/khs26/flu.prmtop")
+    find_backbone_carbons(molecule)
+    for res in sorted(molecule.residues.nodes(), key=lambda x: x.index):
+        print res, res.backbone_map.items()
